58bdd2a6b99acb4607e177d01bc347d55c4932d4
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-01-19 14:08:46 -0600

    Lista enlazada simple

---
+++ ListaEnlazadaSimple/src/listaenlazadasimple/listaSimple.java
@@ -0,0 +1,19 @@
+package listaenlazadasimple;
+
+public class listaSimple {
+    private Nodo primero;
+    private int numElemento;
+
+    public listaSimple(Nodo primero, int numElemento) {
+        this.primero = primero;
+        this.numElemento = numElemento;
+    }
+    
+    public void insertarPrimero(int elemento){
+        
+    }
+    
+    public void insertarFinal(int elemento){
+        
+    }
+}
