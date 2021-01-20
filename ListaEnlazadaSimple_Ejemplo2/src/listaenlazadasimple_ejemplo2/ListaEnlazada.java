53cb6ab6b503e3778c9fc9bcf198d7cdcf307450
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-01-20 15:10:20 -0600

    Lista enlazada ejemplo 2

--- ListaEnlazadaSimple_Ejemplo2/src/listaenlazadasimple_ejemplo2/ListaEnlazada.java
+++ ListaEnlazadaSimple_Ejemplo2/src/listaenlazadasimple_ejemplo2/ListaEnlazada.java
@@ -38,4 +38,40 @@ public class ListaEnlazada {
         }
         return temporal.getValor(); //Regresamos el valor.
     }
+    
+    public void eliminarPrimero(){ /*Este metodo es para elminar el primer valor de la lista es decir
+        la cabeza, la cabeza es el primer valor de la lista.*/
+        cabeza = cabeza.getSiguiente(); //Obtenemos el valor siguiente de la lista y lo reducimos.
+        size--;
+    }
+    
+    public void eliminar(int index){
+        /*1 -> 2 -> 3 -> 4 -> 5
+        1 -> 2 -> 4
+        1 -> 2 -> 4 -> 5*/
+        if(cabeza == null){ //Comprobamos si la cabeza de la lista es nula.
+            cabeza = cabeza.getSiguiente();
+        } else {
+        int contador = 0; //Se crea un contador.
+        Nodo temporal = cabeza; //Creamos un nodo temporal que sea igual a la cabeza.
+        while(contador<index-1){ //Sentencia while para poder eliminar.
+            temporal = temporal.getSiguiente(); //Obtenemos el valor siguiente
+            contador++;
+        }
+        temporal.enlazarSiguiente(temporal.getSiguiente().getSiguiente());
+        }
+        size--; //Disminuimos el valor en -1
+    }
+    
+    public void cortar(int index){ //Metodo para cortar la lista
+        //1 -> 2 -> 3 depues del nodo 3 se borra lo demas.
+        int contador = 0; //Se crea un contador.
+        Nodo temporal = cabeza; //Creamos un nodo temporal que sea igual a la cabeza.
+        while(contador<index-1){ //Sentencia while para poder eliminar.
+            temporal = temporal.getSiguiente(); //Obtenemos el valor siguiente
+            contador++;
+        }
+        temporal.enlazarSiguiente(null); //Encuentra el nodo indicado y de ahi en adelante lo borrao corta.
+        size = index;
+    }
 }
--- ListaEnlazadaSimple_Ejemplo2/src/listaenlazadasimple_ejemplo2/ListaEnlazadaSimple_Ejemplo2.java
+++ ListaEnlazadaSimple_Ejemplo2/src/listaenlazadasimple_ejemplo2/ListaEnlazadaSimple_Ejemplo2.java
@@ -12,16 +12,25 @@ public class ListaEnlazadaSimple_Ejemplo2 {
         
         /*Al momento de imprimir el valor recorreo la lista hasta la tercera posicion ya que obtenemos
         los dos primeros valores de la llsta, si borramos un getSiguiente se obtiene el anterior*/
-        //System.out.println(primer.getSiguiente().getSiguiente().getValor().toString());
+        System.out.println("--CLASE NODO--");
+        System.out.println(primer.getSiguiente().getSiguiente().getValor().toString());
         
         ListaEnlazada lista = new ListaEnlazada(); //Creamos un objeto de la clase listaEnlazada.
-        
+        System.out.println("--CLASE LISTA ENLAZADA--");
         System.out.println("Esta vacia la lista? " + lista.esVacia());
         
-        lista.addPrimero("David"); //Agregamos valores a la lista, cabeza.
-        lista.addPrimero(21);
-        lista.addPrimero("Desarrollador de software");
-        lista.addPrimero(10);
+        lista.addPrimero(7); //Ultimo valor.
+        lista.addPrimero(6); //Agregamos valores a la lista, cabeza.
+        lista.addPrimero(5);
+        lista.addPrimero(4);
+        lista.addPrimero(3);
+        lista.addPrimero(2);
+        lista.addPrimero(1); //Primer valor (cabeza).
+        
+        //lista.eliminarPrimero(); //Eliminar primer elemento de la lista.
+        //lista.eliminar(2); //Eliminar un nodo especifico
+        
+        lista.cortar(3); //Cortar apartir de un nodo especifico.
         
         System.out.println("Primer elemento: " + lista.obtener(0));
         System.out.println("Ultimo elemento: " + lista.obtener(lista.size()-1));
